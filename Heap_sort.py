846da188c141501ff5237bfa2c9f73c74f5b77b7
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2023-05-02 12:15:24 +0530

    Create Heap_sort.py

---
+++ Heap_sort.py
@@ -0,0 +1,34 @@
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n//2 - 1, -1, -1):                                                     # Build a max heap
+        heapify(arr, n, i)
+
+    for i in range(n-1, 0, -1):                                                           # Extract elements from the heap one by one
+        arr[i], arr[0] = arr[0], arr[i]                                                   # Swap the first element (max) with the last element
+        heapify(arr, i, 0)
+
+    return arr
+
+def heapify(arr, n, i):
+    largest = i                                                                           # Initialize largest as root
+    left = 2*i + 1                                                                        # Left child
+    right = 2*i + 2                                                                       # Right child
+
+    if left < n and arr[left] > arr[largest]:                                             # Check if left child is larger than root
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:                                           # Check if right child is larger than largest so far
+        largest = right
+
+    if largest != i:                                                                      # If the largest element is not the root, swap them
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)                                                          # Recursively heapify the affected sub-tree
+
+
+arr = input("Enter a list of numbers separated by spaces: ")
+arr = [int(num) for num in arr.split()]
+
+sorted_arr = heap_sort(arr)
+
+print("Sorted array:", sorted_arr)
