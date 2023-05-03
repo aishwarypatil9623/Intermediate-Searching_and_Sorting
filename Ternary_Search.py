c70053868b8fa0daf4f7aa5321a5849e2d4a18dc
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2023-05-03 13:05:48 +0530

    Update Ternary_Search.py

--- Ternary_Search.py
+++ Ternary_Search.py
@@ -23,3 +23,7 @@ if result == -1:                                                        # If the
     print("Element not found")
 else:                                                                   # If the element is found, print its index
     print(f"Element found at index {result}")
+
+'''OUTPUT
+Element found at index 2
+'''
